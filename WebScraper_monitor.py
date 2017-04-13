7fab84d299325303312a2fc2d7325ad484f77044
Aadit <aaditbhojgi@gmail.com>
2017-04-13 13:49:50 +0530

    Removed some bugs this monitor is according to the URLs used in the project

--- WebScraper_monitor.py
+++ WebScraper_monitor.py
@@ -34,21 +34,32 @@ while True:
         If no content is included yet in the required field then NOT AVAILABLE message is printed and after 1 hour,
         it will again check for the content in the required field until it finds any content.
     """
-    if info is None:
-        print("Requested Page is not available")
+    if info is None and i > 44757:
+        print "Judgement:{} is not available".format(i)  # if there is no content in given URL
 
-        # it will go on sleep for 1 hour and check again the condition( you can change the update time if you want to)
+        """
+            the URL is checked for every 1 hour whether content has been inserted into that particular URL or not
+            (You can also change the sleep time to whatever you want to)
+        """
         time.sleep(3600)
+    elif info is None and i <= 44757:
+        filename = 'Judgement\Judgement{}.txt'.format(i)
+        with open(filename, 'a') as writer:
+            # As there is no data present in the given URL
+            writer.writelines("No CaseStudy Available for judgement: {}".format(i))
+        # Value of index 'i' is incremented to traverse to the Next URL
+        i += 1
 
     # The moment the content is inserted in the URL The content is parsed from the URL and is written to a TEXT file
     else:
-
-        # A text file for the given URL is created in Data folder with name = Data1 or 2 or 3...(i.e. the value of i)
-        filename = 'Data\Data{}.txt'.format(i)
+        # A text file for the given URL is created in Data folder with name = Data1 or 2 or 3..(i.e. the value of i)
+        filename = 'Judgement\Judgement{}.txt'.format(i)
 
         # finally the parsed text content from URL is written to a text file
         with open(filename, 'a') as writer:
-            writer.writelines(str(info.get_text()))
+
+            # unicode present in the file will be converted into string AS there is in judgement 4423
+            writer.writelines(str(info.get_text().encode('utf-8')))
 
         # Value of index 'i' is incremented to traverse to the Next URL
         i += 1
